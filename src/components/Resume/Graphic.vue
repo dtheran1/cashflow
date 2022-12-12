d2f9dc6ba1526332eb6404ec1b3a6bc935be8734
Dtheran <dtheran1@gmail.com>
2022-12-12 12:14:58 -0500

    wip

--- src/components/Resume/Graphic.vue
+++ src/components/Resume/Graphic.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    <svg viewBox="0 0 300 200">
+    <svg
+      @touchstart="tap"
+      @touchmove="tap"
+      @touchend="untap"
+      viewBox="0 0 300 200">
       <line
         stroke="#c4c4c4"
         stroke-width="2"
         x1="0"
-        y1="100"
+        :y1="zero"
         x2="300"
-        y2="100"
+        :y2="zero"
       />
       <polyline
         fill="none"
@@ -16,11 +20,12 @@
         :points="points"
       />
       <line
+        v-show="showPointer"
         stroke="#04b500"
         stroke-width="2"
-        x1="200"
+        :x1="pointer"
         y1="0"
-        x2="200"
+        :x2="pointer"
         y2="200"
       />
     </svg>
@@ -29,7 +34,7 @@
 </template>
 
 <script setup>
-  import { defineProps, toRefs, computed } from 'vue';
+  import { defineProps, toRefs, computed, ref } from 'vue';
   const props = defineProps({
     amounts: {
       type: Array,
@@ -38,23 +43,46 @@
   });
   const { amounts } = toRefs(props);
 
-  const amountTopIXELS = (amount) => {
+  const amountToPx = (amount) => {
     const min = Math.min(...amounts.value);
     const max = Math.max(...amounts.value);
 
-    return `${min}, ${max}`
-  }
-  const points = computed(() => {
-    const totalEl = amounts.value.length;
-    return Array(totalEl)
-      .fill(100)
-      .reduce((points, amount, i) => {
-        const x = (300 / totalEl) * (i + 1);
-        const y = amountTopIXELS(amount);
-        return `${points} ${x},${y}`;
-      }, '0,100');
+    const amountAbs = amount + Math.abs(min);
+    const minmax = Math.abs(max) + Math.abs(min);
+
+    return 200 - ((amountAbs * 100) / minmax) * 2;
+  };
+
+  const zero = computed(() => {
+    return amountToPx(0);
   });
 
+  const points = computed(() => {
+    const totalEl = amounts.value.length;
+    return amounts.value.reduce((points, amount, i) => {
+      const x = (300 / totalEl) * (i + 1);
+      const y = amountToPx(amount);
+      return `${points} ${x},${y}`;
+    }, '0,100');
+  });
+
+  const showPointer = ref(false);
+
+  const pointer = ref(0)
+
+  const tap = ({target, touches}) => {
+    showPointer.value = true;
+    const elWidth = target.getBoundingClientRect().width; // Esta funcion nativa de JS nos permite calcular el width de un elemento cuando se presenta en diferentes anchos de pantalla
+    const elX = target.getBoundingClientRect().x; // Obtenemos el punto inicial del lienzo en el eje x
+    const touchX = touches[0].clientX; // Obtenemos el valor del touch del cliente en el eje x
+
+    pointer.value = ((touchX - elX) * 300) / elWidth;
+  };
+
+  const untap = ({target, touches}) => {
+    showPointer.value = false;
+
+  };
 </script>
 
 <style scoped>
